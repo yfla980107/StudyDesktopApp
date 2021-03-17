9a1c40e9aee1d5cbe1ef4a780dbafef1e51179a7
yfla980107 <yfla980107@gmail.com>
2021-03-17 17:30:52 +0900

    210317 BookRentalShopApp

---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmDivCode.cs
@@ -0,0 +1,225 @@
+﻿using MetroFramework.Forms;
+using MetroFramework;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BookRentalShopApp
+{
+    public partial class FrmDivCode : MetroForm
+    {
+        #region 전역변수 영역
+
+        private bool IsNew = false; // false (수정) true (신규)
+
+        #endregion
+
+        #region 이벤트 영역
+        public FrmDivCode()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDivCode_Load(object sender, EventArgs e)
+        {
+            IsNew = true; // 신규 초기화
+            RefreshData();
+        }
+        private void FrmDivCode_Resize(object sender, EventArgs e)
+        {
+            DgvData.Height = this.ClientRectangle.Height - 90;
+            GrbDetail.Height = this.ClientRectangle.Height - 90;
+        }
+        private void DgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1) // 선택된 값이 존재하면
+            {
+                var selData = DgvData.Rows[e.RowIndex];
+                TxtDivision.Text = selData.Cells[0].Value.ToString();
+                TxtNames.Text = selData.Cells[1].Value.ToString();
+                TxtDivision.ReadOnly = true;
+
+                IsNew = false; // 수정
+
+            }
+        }
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            // Validation 체크
+            if (CheckValidation() == false) return;
+            // 삭제질문
+            if (MetroMessageBox.Show(this, "삭제하시겠습니까?", "삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
+
+            DeleteData();
+            RefreshData();
+            ClearInputs();
+        }
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            // Validation 체크
+            if (CheckValidation() == false) return;
+
+            SaveData();
+            RefreshData();
+            ClearInputs();
+        }
+        #endregion
+
+        #region 커스텀 메서드 영역
+
+        /// <summary>
+        /// 삭제처리 프로세스
+        /// </summary>
+        private void DeleteData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helper.Common.Connstring))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    var query = "";
+
+                    query = " DELETE [dbo].[divtbl] " +
+                            " WHERE [Division] = @Division ;";
+                    cmd.CommandText = query;
+
+                    SqlParameter pDivision = new SqlParameter("@Division", SqlDbType.VarChar, 8);
+                    pDivision.Value = TxtDivision.Text;
+                    cmd.Parameters.Add(pDivision);
+
+                    var result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MetroMessageBox.Show(this, "삭제 성공", "삭제",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MetroMessageBox.Show(this, "삭제 실패", "삭제",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, $"예외발생 : {ex.Message}", "오류", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+        private void RefreshData() // 화면 조회
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helper.Common.Connstring))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    var query = "SELECT [Division] , " +
+                                " [Names] " +
+                                " FROM[dbo].[divtbl] ";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "divtbl");
+
+                    DgvData.DataSource = ds;
+                    DgvData.DataMember = "divtbl";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MetroMessageBox.Show(this, $"예외발생 : {ex.Message}", "오류", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 입력(수정)처리 프로세스
+        /// </summary>
+        private void SaveData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helper.Common.Connstring))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    var query = "";
+
+                    if (IsNew == true) //INSERT
+                    {
+                        query = "INSERT INTO dbo.divtbl " +
+                            " VALUES " +
+                            " (@Division, @Names) ";
+                    }
+                    else //UPDATE
+                    {
+                        query = " UPDATE [dbo].[divtbl] " +
+                            " SET [Names] = @Names " +
+                            " WHERE [Division] = @Division ;";
+                    }
+                    cmd.CommandText = query;
+
+                    SqlParameter pNames = new SqlParameter("@Names", SqlDbType.NVarChar, 45);
+                    pNames.Value = TxtNames.Text;
+                    cmd.Parameters.Add(pNames);
+
+                    SqlParameter pDivision = new SqlParameter("@Division", SqlDbType.VarChar, 8);
+                    pDivision.Value = TxtDivision.Text;
+                    cmd.Parameters.Add(pDivision);
+
+                    var result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MetroMessageBox.Show(this, "저장 성공", "저장",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MetroMessageBox.Show(this, "저장 실패", "저장",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, $"예외발생 : {ex.Message}", "오류", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 입력값 유효성 체크 메서드
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckValidation()
+        {
+            if (string.IsNullOrEmpty(TxtDivision.Text) || string.IsNullOrEmpty(TxtNames.Text))
+            {
+                MetroMessageBox.Show(this, "빈 값은 처리할 수 없습니다.", "경고",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void ClearInputs()
+        {
+            TxtDivision.Text = TxtNames.Text = "";
+            TxtDivision.ReadOnly = false;
+            IsNew = true;
+        }
+        #endregion
+    }
+}
--- WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmLogin.cs
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/FrmLogin.cs
@@ -27,6 +27,8 @@ namespace BookRentalShopApp
 
         private void BtnLogin_Click(object sender, EventArgs e)
         {
+            var strUserId = ""; //query문에 있는 userID값을 받아오는 변수
+
             //MessageBox.Show("로그인 처리!");
             if (string.IsNullOrEmpty(TxtUserId.Text) || string.IsNullOrEmpty(TxtPassword.Text))
             {
@@ -42,14 +44,45 @@ namespace BookRentalShopApp
                 {
                     if (conn.State == ConnectionState.Closed) conn.Open();
 
+                    var query = "SELECT userID FROM membertbl " +
+                        " WHERE userID = @userID " +
+                        " AND passwords = @passwords " +
+                        " AND levels = 'S' ";
+
                     // Sqlcommand 생성
-                    SqlCommand cmd = new SqlCommand();
+                    SqlCommand cmd = new SqlCommand(query, conn);
                     // SqlInjection 해킹 막기위해서 사용
-                    SqlParameter param;
+                    SqlParameter pUserID = new SqlParameter("@userId", SqlDbType.VarChar, 20);
+                    pUserID.Value = TxtUserId.Text;
+                    SqlParameter pPasswords = new SqlParameter("@passwords", SqlDbType.VarChar, 20);
+                    pPasswords.Value = TxtPassword.Text;
+                    cmd.Parameters.Add(pUserID);
+                    cmd.Parameters.Add(pPasswords);
+
                     // SqlDataReader 실행(1)
                     SqlDataReader reader = cmd.ExecuteReader();
-
                     // reader로 처리...
+                    reader.Read();
+                    strUserId = reader["userID"] != null ? reader["userID"].ToString() : "";
+                    reader.Close();
+                    // 확인 MessageBox.Show(strUserId);
+                    if (string.IsNullOrEmpty(strUserId))
+                    {
+                        MetroMessageBox.Show(this, "접속실패", "로그인실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        var updateQuery = $@"UPDATE membertbl SET 
+                                             lastLoginDT = GETDATE()
+                                            ,loginIpAddr = '{Helper.Common.GetLocalIp()}'
+                                            WHERE userId = '{strUserId}' "; // 로그인정보 남기기
+                        cmd.CommandText = updateQuery;
+                        cmd.ExecuteNonQuery();
+                        MetroMessageBox.Show(this, "접속성공", "로그인성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+
+                    }
                 }
             }
             catch (Exception ex)
